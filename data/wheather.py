44c1ac333be6742f36f599f156c66ff8adda1659
WhoouchPlay <stasolenchuk89@gmail.com>
2025-02-02 21:21:31 +0100

    Update

--- data/wheather.py
+++ data/wheather.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_wheather(city: str = "Neratovice"):
-    api_key = os.getenv("a6d53a9618f040caa60193244242912")
+def get_wheather(city: str = "Odesa"):
+    api_key = os.getenv("WHEATHER_API")
     url = f"https://api.weatherapi.com/v1/current.json?q={city}&key={api_key}"
     response = requests.get(url).json()
     wheather = {
